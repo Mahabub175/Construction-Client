d27abc5c7b0516e182e85148ddf788c0c4d80b9d
JefrinAkterJui <jefrin.jui.dev@gmail.com>
2025-03-25 23:43:23 +0600

    About page

---
+++ src/components/LandingPages/About-us/Specialize.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Specialize = () => {
+  return (
+    <>
+        <section>
+            <div className="my-container py-[60px] lg:py-[100px]">
+                <div className="flex md:gap-[60px] xl:gap-[100px] justify-center items-center md:flex-row sm:flex-col-reverse">
+                    <div className="firt-p-text">
+                        <h1 className='text-[20px] md:text-[16px] lg:text-[24px] font-[400] leading-[38px] lg:w-[475px] md:w-[300px] lg:tracking-[8px] md:tracking-[4px]  lg:[word-spacing:10px]  md:text-start text-center '>A COMPANY <span className=' text-primary'>SPECIALIZED</span> ON YOU</h1>
+                        <p className='sm:text-[10px] md:text-[14px] lg:text-[16px] xl:text-[18px] text-black font-[400] lg:leading-[28px] md:leading-[16px] lg:w-[485px] md:w-[300px] xl:w-[600px] mt-[20px] md:text-start text-center'>Genesis Carpentry is a custom woodwork company located in Bangladesh. For generations, superior craftsmanship has been a tradition in the Martinez family. Today, Roberto & Robeisy Martinez, third and fourth-generation carpenters, proudly offer some of the finest handcrafted woodwork.</p>
+                        <p className='sm:text-[10px] md:text-[14px] lg:text-[16px] xl:text-[18px] text-black font-[400] lg:leading-[28px] md:leading-[16px] lg:w-[485px] md:w-[300px] xl:w-[600px] lg:mt-[20px] md:mt-[16px] mt-[10px] md:text-start text-center'>For several years Genesis Carpentry has provided its services to a wide variety of Luxury Residences, Hotels & Condominium, Restaurants, Stores and many more customers throughout South Florida and the Caribbean.</p>
+                    </div>
+                    <div className="first-p-imge">
+                        <div className='about-imge lg:w-[354px] lg:h-[480px] md:w-[250px] md:h-[338px]'></div>
+                    </div>
+                </div>
+                <div className="flex md:gap-[60px] xl:gap-[100px] justify-center items-center md:flex-row sm:flex-col-reverse">
+                    <div className="second-p-imge">
+                        <div className='about-imge lg:w-[354px] lg:h-[480px] md:w-[250px] md:h-[338px]'></div>
+                    </div>
+                    <div className="firt-p-text">
+                        {/* <h1 className='text-[20px] md:text-[16px] lg:text-[24px] font-[400] leading-[38px] lg:w-[475px] md:w-[300px] lg:tracking-[8px] md:tracking-[4px]  lg:[word-spacing:10px]  md:text-start text-center '>A COMPANY <span className=' text-primary'>SPECIALIZED</span> ON YOU</h1> */}
+                        <p className='sm:text-[10px] md:text-[14px] lg:text-[16px] xl:text-[18px] text-black font-[400] lg:leading-[28px] md:leading-[16px] lg:w-[485px] md:w-[300px] xl:w-[600px] mt-[20px] md:text-start text-center'>Some of our operations include: design services, consultation, custom fabrication, installation and completion. Our experience and capabilities provide us with ability to work with home and business owners, architects and designers. We work with contractors during the process, developing drawings, creating and providing samples for our customer. Outstanding customer service is our goal at Robert Carpentry, We live by the rules that customer satisfaction is our primary and foremost goal.</p>
+                        <p className='sm:text-[10px] md:text-[14px] lg:text-[16px] xl:text-[18px] text-black font-[400] lg:leading-[28px] md:leading-[16px] lg:w-[485px] md:w-[300px] xl:w-[600px] lg:mt-[20px] md:mt-[16px] mt-[10px] md:text-start text-center'>We invite you to work together with us on your next project, you will experience the difference in quality, service, and professionalism. Please do not hesitate to contact us, a member of our family will gladly assist you with any questions you may have.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Specialize
