eb69f24ba30c92dcd0a507cf60a3e292dca123e5
Astro <rgrxmahabub.official@gmail.com>
2025-04-29 23:13:58 +0600

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: [
       "localhost",
       "thumbs.dreamstime.com",
-      "api.photohousemagazine.com",
+      "genesisapi.moonsgallerysystem.com",
     ],
   },
 };
