d86074cdf7cd2e9d4e92729a46fd7a369e2a8cc5
Astro <rgrxmahabub.official@gmail.com>
2025-04-29 22:17:09 +0600

    Updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "thumbs.dreamstime.com", "taskapi.devmahabub.com"],
+    domains: [
+      "localhost",
+      "thumbs.dreamstime.com",
+      "api.photohousemagazine.com",
+    ],
   },
 };
 
