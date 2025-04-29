6f5dee17bc167f8318c0b7ae2af7b649b50e0bea
Astro <rgrxmahabub.official@gmail.com>
2025-04-29 23:17:13 +0600

    Updated

--- src/utilities/configs/base_api.js
+++ src/utilities/configs/base_api.js
@@ -1,5 +1,5 @@
-export const base_url = "https://genesisapi.moonsgallerysystem/api/v1";
-export const base_url_image = "https://genesisapi.moonsgallerysystem/";
+export const base_url = "https://genesisapi.moonsgallerysystem.com/api/v1";
+export const base_url_image = "https://genesisapi.moonsgallerysystem.com/";
 // export const base_url = "http://localhost:8000/api/v1";
 // export const base_url_image = "http://localhost:8000/";
 // export const base_url = "http://192.168.1.6:5000/api/v1";
