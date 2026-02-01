989bb8ba16d01521ef84b89ac28df34c8d74b517
Mahabub175 <rgrxmahabub.official.com>
2026-02-01 14:32:37 +0600

    Updated

--- src/components/LandingPages/Process/ProcessSteps.jsx
+++ src/components/LandingPages/Process/ProcessSteps.jsx
@@ -18,38 +18,41 @@ const ProcessSteps = () => {
         Our process makes it easy.
       </h3>
       <div className="lg:mt-20">
-        {stepData?.results?.map((item, i) => (
-          <div
-            key={item?.id}
-            className={`flex flex-col items-center ${
-              i % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
-            } justify-between gap-10 mb-10`}
-          >
-            <div className="lg:w-3/6">
-              <h5 className="mb-2 font-medium">{item?.heading}</h5>
-              <h3 className="font-medium text-xl lg:text-3xl mb-3 tracking-widest">
-                {item?.title}
-              </h3>
-              <p className="text-gray-600 leading-relaxed mb-6">
-                {item?.description}
-              </p>
-              <ul className="list-disc list-inside pl-5">
-                {item?.list?.map((list, i) => (
-                  <li key={i} className="mb-2">
-                    {list}
-                  </li>
-                ))}
-              </ul>
-            </div>
+        {stepData?.results
+          ?.slice()
+          ?.sort((a, b) => a.order - b.order)
+          ?.map((item, i) => (
+            <div
+              key={item?.id}
+              className={`flex flex-col items-center ${
+                i % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
+              } justify-between gap-10 mb-10`}
+            >
+              <div className="lg:w-3/6">
+                <h5 className="mb-2 font-medium">{item?.heading}</h5>
+                <h3 className="font-medium text-xl lg:text-3xl mb-3 tracking-widest">
+                  {item?.title}
+                </h3>
+                <p className="text-gray-600 leading-relaxed mb-6">
+                  {item?.description}
+                </p>
+                <ul className="list-disc list-inside pl-5">
+                  {item?.list?.map((list, i) => (
+                    <li key={i} className="mb-2">
+                      {list}
+                    </li>
+                  ))}
+                </ul>
+              </div>
 
-            <Image
-              src={item?.attachment}
-              alt={item?.title}
-              width={500}
-              height={500}
-            />
-          </div>
-        ))}
+              <Image
+                src={item?.attachment}
+                alt={item?.title}
+                width={500}
+                height={500}
+              />
+            </div>
+          ))}
       </div>
     </section>
   );
